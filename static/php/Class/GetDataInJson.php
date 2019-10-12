e20d95e00b6266b094260e27feae060ebdd38ea0
InvoQiu <364482529@qq.com>
2019-10-12 16:39:05 +0800

    'css'

---
+++ static/php/Class/GetDataInJson.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: InvoQiu
+ * Date: 2019/10/11
+ * Time: 11:13
+ */
+
+class GetDataInJson
+{
+
+}
---
+++ static/php/Json.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: InvoQiu
+ * Date: 2019/10/11
+ * Time: 10:53
+ */
+
+
+//初始化Json数据
+$DataJson="";
+
+if($DataArray!=null&&$DataJson!="")
+{
+    $DataArray=json_decode($DataJson);
+}
+
+
